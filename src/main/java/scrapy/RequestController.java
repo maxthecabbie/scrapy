59e2aed04a43afe6c405487a24248e6fc256f90b
Ryan Ho <maxthecabbie@gmail.com>
2017-11-11 15:55:34 -0800

    Change RequestController to use POST request
    Yelp URL is processed through JSON body

--- src/main/java/scrapy/RequestController.java
+++ src/main/java/scrapy/RequestController.java
@@ -3,18 +3,34 @@ package scrapy;
 import scrapy.yelpscraper.YelpRequestController;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import java.util.ArrayList;
-import java.io.IOException;
+import java.util.HashMap;
+
 
 @RestController
 public class RequestController {
 
-    @RequestMapping(value = "/")
-    public ArrayList<String> index() {
-        YelpRequestController yelpRequest = new YelpRequestController("https://www.yelp.ca/biz/l-industrie-pizzeria-brooklyn");
+    @PostMapping(value = "/")
+    public ArrayList<String> index(@RequestBody String reqBodyString) {
+        HashMap<String, String> requestBody = parseReqBodyString(reqBodyString);
+        String yelpURL = requestBody.get("yelpURL");
+        YelpRequestController yelpRequest = new YelpRequestController(yelpURL);
         ArrayList<String> yelpImageLinks = yelpRequest.makeYelpRequest();
         return yelpImageLinks;
+    }
+
+    private HashMap<String, String> parseReqBodyString(String reqBodyString) {
+        HashMap<String, String> requestBody = new HashMap<>();
+        Gson gson = new Gson();
+        JsonObject reqBodyJsonObj = gson.fromJson(reqBodyString, JsonObject.class);
+
+        JsonElement url = reqBodyJsonObj.get("yelpURL");
+        requestBody.put("yelpURL", url.getAsString());
+        return requestBody;
     }
 }
